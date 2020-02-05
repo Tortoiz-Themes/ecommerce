4e14aaf14fe36a6ea335fc608960dad182000db3
rakesh3899 <rakeshmistry3899@gmail.com>
2020-02-05 13:45:58 +0600

    User,Customer,redirect middleware setup

--- app/Http/Middleware/CustomerMiddleware.php
+++ app/Http/Middleware/CustomerMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerMiddleware
 {
@@ -15,6 +16,13 @@ class CustomerMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::check() && Auth::user()->role->id == 2)
+        {
+            return $next($request);
+        }
+        else
+        {
+            return redirect()->route('login');
+        }
     }
 }
